103f2ef1fc5a646b3e35562cb175a2cec9aff34a
Iorga Sebastian George <sebyiorga95@yahoo.com>
2024-02-17 23:20:03 +0200

    Snapshot 1

--- LaneDetection.cpp
+++ LaneDetection.cpp
@@ -1,16 +1,18 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
-#include <armadillo>
 #include <vector>
 #include <iostream>
 
-cv::Mat preprocessImage(cv::Mat image);
-cv::Mat getImageROI(cv::Mat image);
-cv::Mat getLaneLinesImage(cv::Mat image, cv::Mat original, std::vector<cv::Vec4i>& lines);
-cv::Vec4i getCoordinates(cv::Size imageSize, cv::Vec2d lineSif);
-std::vector<cv::Vec4i> averageSlopeInterceptForm(cv::Size imageSize, std::vector<cv::Vec4i> lines);
-void displayLane(cv::Mat image, std::vector<cv::Point> lane);
+
+cv::Mat perspectiveMapping(cv::Mat image, std::vector<cv::Point> cameraLanePoints, bool inverse);
+cv::Mat applyThreshold(cv::Mat image);
+std::pair<int, int> getLinesStartingPosition(cv::Mat image);
+std::vector<cv::Point> getLanePoints(cv::Mat image, std::pair<int, int> linesStartingPosition, int& leftPointsCount, int& rightPointsCount);
+float getRelativeDistanceToLaneCenter(std::vector<cv::Point> lanePoints, int leftPointsCount, int rightPointsCount);
+void displayLane(cv::Mat image, std::vector<cv::Point> cameraLanePoints, std::vector<cv::Point> lanePoints);
+cv::Mat detectLaneImage(cv::Mat image);
+
 
 int main() {
 	uint16_t testcase;
@@ -20,26 +22,12 @@ int main() {
 	switch (testcase) {
 	case 1: {
 		// Read image
-		cv::Mat image = cv::imread("image1.jpg");
-		cv::resize(image, image, cv::Size(1200, 700));
+		cv::Mat image = cv::imread("Images/image1.jpg");
+		cv::resize(image, image, cv::Size(1280, 720));
+		
+		cv::Mat resultImage = detectLaneImage(image);
 
-		cv::Mat imageProc = preprocessImage(image);
-		cv::imshow("Image Proc", imageProc);
-		cv::Mat imageMask = getImageROI(imageProc);
-		cv::imshow("Image Mask", imageMask);
-
-		// Get lane lines using Hough Transform technique
-		std::vector<cv::Vec4i> lines;
-		cv::HoughLinesP(imageMask, lines, 2, CV_PI / 180, 100, 40, 5);
-
-		std::vector<cv::Vec4i> linesAvg = averageSlopeInterceptForm(image.size(), lines);
-		cv::Mat imageLines = getLaneLinesImage(imageMask, image, linesAvg);
-
-		// Display lane on original image
-		cv::Mat imageLane;
-		cv::addWeighted(image, 0.8, imageLines, 1, 1, imageLane);
-
-		cv::imshow("Image", imageLane);
+		cv::imshow("Image", resultImage);
 		cv::waitKey(0);
 
 		break;
@@ -47,34 +35,21 @@ int main() {
 	case 2: {
 		// Read video
 		int videoId = 0;
-		while (!(videoId >= 1 && videoId <= 2)) {
-			std::cout << "Please choose the video ID (a number between 1 and 2): ";
+		while (!(videoId >= 1 && videoId <= 3)) {
+			std::cout << "Please choose the video ID (a number between 1 and 3): ";
 			std::cin >> videoId;
 		}
-		cv::VideoCapture cap("video" + std::to_string(videoId) + ".mp4");
+
+		cv::VideoCapture cap("Videos/video" + std::to_string(videoId) + ".mp4");
 		while (cap.isOpened()) {
-			cv::Mat image; // Frame
-			cap.read(image);
-			cv::resize(image, image, cv::Size(1200, 700));
+			cv::Mat frame;
+			cap.read(frame);
+			cv::resize(frame, frame, cv::Size(1280, 720));
 
-			cv::Mat imageProc = preprocessImage(image);
-			cv::Mat imageMask = getImageROI(imageProc);
+			cv::Mat resultFrame = detectLaneImage(frame);
 
-			// Get lane lines using Hough Transform technique
-			std::vector<cv::Vec4i> lines;
-			cv::HoughLinesP(imageMask, lines, 2, CV_PI / 180, 100, 40, 5);
-
-			std::vector<cv::Vec4i> linesAvg = averageSlopeInterceptForm(image.size(), lines);
-			cv::Mat imageLines = getLaneLinesImage(imageMask, image, linesAvg);
-
-			// Display lane on original image
-			cv::Mat imageLane = image;
-
-			std::vector<cv::Point> lane = { cv::Point(linesAvg[0][0], linesAvg[0][1]), cv::Point(linesAvg[0][2], linesAvg[0][3]), cv::Point(linesAvg[1][2], linesAvg[1][3]), cv::Point(linesAvg[1][0], linesAvg[1][1]) };
-			displayLane(imageLane, lane);
-
-			cv::imshow("Image", imageLane);
-			if (cv::waitKey(15) == 'q') break;
+			cv::imshow("Video", resultFrame);
+			if (cv::waitKey(10) == 'q') break;
 		}
 
 		cap.release();
@@ -84,129 +59,194 @@ int main() {
 	}
 	default:
 		std::cout << "Fair enough." << std::endl;
+
 		break;
 	}
 	
 	return 0;
 }
 
-cv::Mat preprocessImage(cv::Mat image) {
-	cv::Mat imageProc;
-	cv::cvtColor(image, imageProc, cv::COLOR_RGB2GRAY); // Converts to grayscale
-	cv::GaussianBlur(imageProc, imageProc, cv::Size(5, 5), 0); // Reduces image noise
-	cv::Canny(imageProc, imageProc, 50, 150); // Identifies edges
-	return imageProc;
+
+
+cv::Mat perspectiveMapping(cv::Mat image, std::vector<cv::Point> cameraLanePoints, bool inverse) {
+	cv::Mat imageWarp;
+
+	std::vector<cv::Point2f> src = { cv::Point2f(cameraLanePoints[0].x, cameraLanePoints[0].y), cv::Point2f(cameraLanePoints[3].x, cameraLanePoints[3].y),
+		cv::Point2f(cameraLanePoints[1].x, cameraLanePoints[1].y), cv::Point2f(cameraLanePoints[2].x, cameraLanePoints[2].y) };
+	
+	std::vector<cv::Point2f> dest = { cv::Point2f(200, 0), cv::Point2f(200, 720), cv::Point2f(1080, 0), cv::Point2f(1080, 720) };
+
+	cv::Mat perspectiveMatrix;
+	if (inverse) perspectiveMatrix = cv::getPerspectiveTransform(src, dest);
+	else perspectiveMatrix = cv::getPerspectiveTransform(dest, src);
+
+	cv::warpPerspective(image, imageWarp, perspectiveMatrix, cv::Size(1280, 720));
+
+	return imageWarp;
 }
 
-cv::Mat getImageROI(cv::Mat image) {
-	// The region of interest is the lane the car is currently on, which if contoured from the camera's perspective
-	// until it is out of sight, resembles a triangle.
 
-	// Using that information, we can create a mask by defining a matrix of zeros, which size is equal to our image
-	// size, then changing the values of the elements situated at the indexes of our filled triangle to 255 (white).
 
-	cv::Size imageSize = image.size();
-	int imageHeight = imageSize.height, imageWidth = imageSize.width;
-	std::vector<cv::Point> triangle = { cv::Point(200, imageHeight), cv::Point(1100, imageHeight), cv::Point(550, 250) };
-	cv::Mat mask(imageSize, CV_8UC1, cv::Scalar(0, 0, 0));
-	cv::fillPoly(mask, triangle, cv::Scalar(255, 255, 255));
+cv::Mat applyThreshold(cv::Mat image) {
+	cv::Mat imageThres, redChannel, sobel;
+	
+	std::vector<cv::Mat> channels;
+	cv::split(image, channels);
+	redChannel = channels[2];
+	cv::GaussianBlur(redChannel, redChannel, cv::Size(5, 5), 0);
+	cv::threshold(redChannel, redChannel, 190, 255, cv::THRESH_BINARY);
 
-	// Using the bitwise and operator, we can mask by comparing the binary representation of the values of our matrixes
-	// located at the same indexes bit by bit, and storing the result of the and operator in an output matrix at the
-	// same indexes as the ones' of our elements.
-
-	// I.e. if we have a pixel in our image whose value is 135 (in binary it's 10000111), and at the same indexes 
-	// in our mask we have the value 255 (11111111), by using the bitwise and operator, the result will be the pixel value;
-	// if we are to compare the same pixel, but to the value of 0, the result will be 0.
-
-	// Therefore, since the pixels that are in our region of interest are compared to 255 and the ones' outside of it
-	// are compared to 0, the output matrix will only hold the pixel values' for our region of interest, with the rest of
-	// the elements being zero.
-
-	cv::Mat imageMask;
-	cv::bitwise_and(image, mask, imageMask);
-
-	return imageMask;
+	return redChannel;
 }
 
-cv::Mat getLaneLinesImage(cv::Mat image, cv::Mat original, std::vector<cv::Vec4i>& lines) {
-	cv::Mat imageLines(image.size(), original.type(), cv::Scalar(0, 0, 0));
-	// For each line, 
-	if (!lines.empty()) {
-		for (auto line : lines) {
-			cv::line(imageLines, cv::Point(line[0], line[1]), cv::Point(line[2], line[3]), cv::Scalar(255, 0, 255), 10);
+
+
+std::pair<int, int> getLinesStartingPosition(cv::Mat image) {
+	cv::Size size = image.size();
+
+	// Make histogram
+	std::vector<int> histogram(1280, 0);
+	for (int x = 0; x < size.width; x++) {
+		for (int y = 0; y < size.height; y++) {
+			if (image.at<uchar>(y, x) == 255) histogram[x]++;
 		}
 	}
 
-	return imageLines;
-}
-
-cv::Vec4i getCoordinates(cv::Size imageSize, cv::Vec2d lineSif) {
-	// Get the coordinates of the two points bounding the line segment
-	double slope = lineSif[0], intercept = lineSif[1];
-	int y1 = imageSize.height; // The Y of the first point is the bottom of the image
-	int y2 = y1 * (3.0 / 5.0); // The Y of the second point is 3/5 * bottom of the image in order to 
-	// From slope intercept form Y = m * X + n we get X = (Y - n) / m, where m is the slope and n is the intercept
-	int x1 = (y1 - intercept) / slope;
-	int x2 = (y2 - intercept) / slope;
-	return cv::Vec4i(x1, y1, x2, y2);
-}
-
-std::vector<cv::Vec4i> averageSlopeInterceptForm(cv::Size imageSize, std::vector<cv::Vec4i> lines) {
-	// The slope intercept form (sif) of a line is the line equation of form Y = m * X + n, where m is the slope of the line and n
-	// is the intercept.
-	std::vector<cv::Vec2d> leftSifVec, rightSifVec;
-
-	for (auto line : lines) {
-		double x1 = line[0], y1 = line[1], x2 = line[2], y2 = line[3];
-		arma::vec X = { x1, x2 };
-		arma::vec Y = { y1, y2 };
-
-		// Fit a linear polynomial (order 1)
-		arma::vec coefficients = arma::polyfit(X, Y, 1);
-
-		// Extract slope and intercept
-		double slope = coefficients[0];
-		double intercept = coefficients[1];
-
-		if (slope >= 0) {
-			rightSifVec.push_back(cv::Vec2d(slope, intercept));
-		}
-		else {
-			leftSifVec.push_back(cv::Vec2d(slope, intercept));
+	// Get the probable start position of the left line and right line
+	int leftLineP = 0, rightLineP = 1279, leftMax = -1, rightMax = -1;
+	for (int i = 0; i < 640; i++) {
+		if (histogram[i] >= leftMax) {
+			leftMax = histogram[i];
+			leftLineP = i;
 		}
 	}
 
-	double leftSlopeAvg = 0, leftInterceptAvg = 0, rightSlopeAvg = 0, rightInterceptAvg = 0;
-
-	// Calculate the average slope and intercept for the left and right line
-	for (auto elem : leftSifVec) {
-		leftSlopeAvg += elem[0];
-		leftInterceptAvg += elem[1];
+	for (int i = 640; i < 1280; i++) {
+		if (histogram[i] >= rightMax) {
+			rightMax = histogram[i];
+			rightLineP = i;
+		}
 	}
-	leftSlopeAvg /= leftSifVec.size();
-	leftInterceptAvg /= leftSifVec.size();
 
-	for (auto elem : rightSifVec) {
-		rightSlopeAvg += elem[0];
-		rightInterceptAvg += elem[1];
-	}
-	rightSlopeAvg /= rightSifVec.size();
-	rightInterceptAvg /= rightSifVec.size();
-
-	cv::Vec2d leftAvgSif(leftSlopeAvg, leftInterceptAvg);
-	cv::Vec2d rightAvgSif(rightSlopeAvg, rightInterceptAvg);
-
-	// Compute
-	cv::Vec4i leftLine = getCoordinates(imageSize, leftAvgSif);
-	cv::Vec4i rightLine = getCoordinates(imageSize, rightAvgSif);
-
-	return { leftLine, rightLine };
+	return std::pair<int, int>(leftLineP, rightLineP);
 }
 
-void displayLane(cv::Mat image, std::vector<cv::Point> lane) {
+
+
+std::vector<cv::Point> getLanePoints(cv::Mat image, std::pair<int, int> linesStartingPosition, int& leftPointsCount, int& rightPointsCount) {
+	int y = 720;
+
+	std::pair<int, int> linesPosition = linesStartingPosition;
+	std::vector<cv::Point> lanePoints;
+
+	while (y >= 50) {
+		cv::Mat imageCrop;
+
+		// Left line
+		imageCrop = image(cv::Rect(cv::Point2i(std::max(linesPosition.first - 50, 0), y - 50), cv::Point2i(std::min(linesPosition.first + 50, 1280), y)));
+
+		std::vector<std::vector<cv::Point>> contours;
+		cv::findContours(imageCrop, contours, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
+		bool foundLeftLanePoint = false, foundRightLanePoint = false; // If there was no point pushed in the lanePoints vector during contour iteration, use previous line position value
+
+		for (auto contour : contours) {
+			cv::Moments moments = cv::moments(contour);
+			if (moments.m00 != 0) {
+				int cx = moments.m10 / moments.m00;
+				int cy = moments.m01 / moments.m00;
+
+				linesPosition.first = linesPosition.first - 50 + cx;
+				lanePoints.push_back(cv::Point(linesPosition.first, y));
+				foundLeftLanePoint = true;
+				leftPointsCount++;
+			}
+		}
+		if (!foundLeftLanePoint) { lanePoints.push_back(cv::Point(linesPosition.first, y)); leftPointsCount++; }
+
+		// Right line
+		imageCrop = image(cv::Rect(cv::Point2i(std::max(linesPosition.second - 50, 0), y - 50), cv::Point2i(std::min(linesPosition.second + 50, 1280), y)));
+
+		contours.clear();
+		cv::findContours(imageCrop, contours, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
+
+		for (auto contour : contours) {
+			cv::Moments moments = cv::moments(contour);
+			if (moments.m00 != 0) {
+				int cx = moments.m10 / moments.m00;
+				int cy = moments.m01 / moments.m00;
+
+				linesPosition.second = linesPosition.second - 50 + cx;
+				lanePoints.push_back(cv::Point(linesPosition.second, y));
+				foundRightLanePoint = true;
+				rightPointsCount++;
+			}
+		}
+		if (!foundRightLanePoint) { lanePoints.push_back(cv::Point(linesPosition.second, y)); rightPointsCount++; }
+
+
+		cv::rectangle(image, cv::Rect(cv::Point2i(linesPosition.first - 50, y - 50), cv::Point2i(linesPosition.first + 50, y)), cv::Scalar(255, 255, 255), 3);
+		cv::rectangle(image, cv::Rect(cv::Point2i(linesPosition.second - 50, y - 50), cv::Point2i(linesPosition.second + 50, y)), cv::Scalar(255, 255, 255), 3);
+
+		y -= 50;
+	}
+
+	return lanePoints;
+}
+
+
+
+float getRelativeDistanceToLaneCenter(std::vector<cv::Point> lanePoints, int leftPointsCount, int rightPointsCount) {
+	float averageLeftPosition = 0, averageRightPosition = 0;
+
+	for (int i = 0; i < leftPointsCount + rightPointsCount - 1; i++) {
+		if (i < leftPointsCount) averageLeftPosition += lanePoints[i].x;
+		else averageRightPosition += lanePoints[i].x;
+	}
+
+	averageLeftPosition /= leftPointsCount;
+	averageRightPosition /= rightPointsCount;
+
+	return ( ( averageLeftPosition + averageRightPosition ) / 2.0f - 640.0f ) / 10.0f;
+}
+
+
+
+void displayLane(cv::Mat image, std::vector<cv::Point> cameraLanePoints, std::vector<cv::Point> lanePoints) {
+	cv::Mat laneImage = cv::Mat::zeros(cv::Size(1280, 720), CV_8UC3);
+
+	std::vector<cv::Point> hullPoints;
+	cv::convexHull(lanePoints, hullPoints);
+	cv::fillPoly(laneImage, hullPoints, cv::Scalar(0, 255, 0));
+
+	laneImage = perspectiveMapping(laneImage, cameraLanePoints, false);
+	
 	double opacity = 0.50;
-	cv::Mat layer = cv::Mat::zeros(image.size(), CV_8UC3);
-	cv::fillPoly(layer, lane, cv::Scalar(0, 0, 255));
-	cv::addWeighted(image, 1, layer, 1 - opacity, 0, image);
+	cv::addWeighted(image, 1, laneImage, 1 - opacity, 0, image);
+}
+
+
+
+cv::Mat detectLaneImage(cv::Mat image) {
+	// Different camera positions on the car result different perspectives of the road.
+	// If testing using your own or other videos, please change the variables of the points in cameraLanePoints vector.
+	std::vector<cv::Point> cameraLanePoints = { cv::Point(565, 450), cv::Point(735, 450), cv::Point(1260, 700), cv::Point(100, 700) };
+
+	cv::Mat imageWarp = perspectiveMapping(image, cameraLanePoints, true);
+
+	cv::Mat imageThres = applyThreshold(imageWarp);
+
+	std::pair<int, int> linesStartingPosition = getLinesStartingPosition(imageThres); // First integer is left line's position, second integer is right line's position
+
+	int leftPointsCount = 0, rightPointsCount = 0;
+	std::vector<cv::Point> lanePoints = getLanePoints(imageThres, linesStartingPosition, leftPointsCount, rightPointsCount);
+
+	float relativeDistanceToLaneCenter = getRelativeDistanceToLaneCenter(lanePoints, leftPointsCount, rightPointsCount);
+	std::string direction;
+	if (relativeDistanceToLaneCenter < 0) direction = "LEFT";
+	else direction = "RIGHT";
+	cv::putText(image, "REL DISTANCE TO CENTER: " + std::to_string(std::abs(relativeDistanceToLaneCenter)) + " TO THE " + direction, cv::Point(10, 30), cv::FONT_HERSHEY_PLAIN, 1.3, cv::Scalar(0, 255, 0), 2);
+
+	cv::imshow("image thres", imageThres);
+	displayLane(image, cameraLanePoints, lanePoints);
+	return image;
 }
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ### OpenCV C++ Lane Detection
 
-Lane Detection project in C++ using OpenCV (https://opencv.org/) and Armadillo (https://arma.sourceforge.net/)
+Lane Detection project in C++ using OpenCV (https://opencv.org/)
+
+[![CHECK THIS VIDEO](https://img.youtube.com/vi/GMeK9Zt_thA/maxresdefault.jpg)](https://www.youtube.com/watch?v=GMeK9Zt_thA)
